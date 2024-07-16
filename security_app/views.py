565f006d08635472ec5a9b3ea73ecc2fa70e31b9
giokhomaa <giokhomaa@gmail.com>
2024-07-16 23:05:57 +0400

    Update settings

--- security_app/views.py
+++ security_app/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Channel
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import ChannelSerializer
-import uuid
 
 
 class ChannelViewSet(ModelViewSet):
     queryset = Channel.objects.all()
     serializer_class = ChannelSerializer
+    authentication_classes = [SessionAuthentication, JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
